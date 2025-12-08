968dd4019f6d98d6cb7c3a588e89828d79a28c9e
Mouss-L <moussalidya07@gmail.com>
2025-12-08 10:05:52 +0300

    monpremierjavascript

---
+++ script.js
@@ -0,0 +1,19 @@
+// Affichage d'un message dans la console du navigateur
+console.log("Bonjour");
+
+// Déclaration d'une variable
+let salutation = "bonjour tout le monde";
+
+// Affichage de la variable dans la console
+console.log(salutation);
+
+
+// Déclaration de deux variables pour le nom et le prénom
+let nom = "Moussa";
+let prenom = "Lidya";
+
+
+// Affichage du nom complet dans la console
+console.log("Je m'appelle " + nom + " " + prenom);
+
+console.log("Je suis ",nom ,prenom)
