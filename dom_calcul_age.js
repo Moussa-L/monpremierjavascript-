e544cc92a4b406e089fc1b5d384a3cc2442c54c8
Moussa lidya <moussalidya07@gmail.com>
2026-01-06 10:22:42 +0100

    js

--- dom_calcul_age.js
+++ dom_calcul_age.js
@@ -88,5 +88,18 @@ elementImg.className="image-tigre";
 elementImg.classList.add("image","image3");
 elementBody.appendChild(elementImg);
 
-// elementInputNom.addEventListener 
+
+const date = new Date();
+console.log("Ma date :",date);
+
+
+let aneeEnCours = date.getFullYear();
+console.log("Année en cours :",aneeEnCours);
+
+let moisEnCours = date.getMonth();
+console.log("Mois en cours :",moisEnCours);
+
+let jourEnCours = date.getDate();
+console.log("Jour en cours :",jourEnCours);
+
 
